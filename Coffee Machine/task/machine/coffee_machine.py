f22e7306f52af087d2fde717cd0e807cb16e8e11
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2021-08-30 20:50:38 +0200

    Stage 5

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -10,12 +10,13 @@ def coffee_machine_status(machine_supply):
                                                                           machine_supply['cash']))
 
 
-def substract_supply(supply, water, milk, beans, cups, cash):
-    supply['water'] -= water
-    supply['milk'] -= milk
-    supply['beans'] -= beans
-    supply['cups'] -= cups
-    supply['cash'] += cash
+def substract_supply(supply, coffee):
+    list = coffee
+    supply['water'] -= list[0]
+    supply['milk'] -= list[1]
+    supply['beans'] -= list[2]
+    supply['cups'] -= list[3]
+    supply['cash'] += list[4]
 
 
 def action_choice():
@@ -24,11 +25,13 @@ def action_choice():
         if action == 'buy':
             buy_action(machine_supply)
         elif action == 'fill':
-            w_to_fill = int(input('Write how many ml of water you want to add:'))
-            m_to_fill = int(input('Write how many ml of milk you want to add:'))
-            b_to_fill = int(input('Write how many grams of beans you want to add:'))
-            c_to_fill = int(input('Write how many disposable cups you want to add:'))
-            substract_supply(machine_supply, (w_to_fill * -1), (m_to_fill * -1), (b_to_fill * -1), (c_to_fill * -1), 0)
+            fill_list = []
+            fill_list.append(int(input('Write how many ml of water you want to add:')) * -1)
+            fill_list.apppend(int(input('Write how many ml of milk you want to add:')) * -1)
+            fill_list.append(int(input('Write how many grams of beans you want to add:')) * -1)
+            fill_list.append(int(input('Write how many disposable cups you want to add:')) * -1)
+            fill_list.append(0)
+            substract_supply(machine_supply, fill_list)
         elif action == 'take':
             print('I gave you ${}'.format(machine_supply['cash']))
             machine_supply['cash'] = 0
@@ -40,18 +43,63 @@ def action_choice():
             print('Chose wrong action')
 
 #  implement checking if there is enough resources to make coffee
-def check_supply():
-    pass
+def check_supply(choice):
+    values = list(machine_supply.values())
+    counter = 0
+    if choice == '1':
+        espr = ammount_for_coffee('1')
+        for x in range(len(espr)):
+            if values[x] >= espr[x]:
+                counter += 1
+        if counter == 5:
+            return 1
+        else:
+            return 0
+
+    elif choice == '2':
+        latt = ammount_for_coffee('2')
+        for x in range(len(latt)):
+            if values[x] >= latt[x]:
+                counter += 1
+        if counter == 5:
+             return 1
+        else:
+            return 0
+    elif choice == '3':
+        capp = ammount_for_coffee('3')
+        for x in range(len(capp)):
+            if values[x] >= capp[x]:
+                counter += 1
+        if counter == 5:
+             return 1
+        else:
+            return 0
+
+def ammount_for_coffee(number):
+    if number == '1':
+        espresso = [250, 0 , 16, 1, 4]
+        return espresso
+    elif number == '2':
+        latte = [350, 75, 20, 1, 7]
+        return latte
+    elif number == '3':
+        cappuccino = [200, 100, 12 ,1, 6]
+        return cappuccino
 
 
 def buy_action(supply):
     choice = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ')
     if choice == '1':
-        substract_supply(supply, 250, 0, 16, 1, 4)
+        check = check_supply(choice)
+        if  check == 1:
+            substract_supply(supply, ammount_for_coffee(choice))
+            print('I have enough resources, making you a coffee!')
+        else:
+            print('Not enough')
     elif choice == '2':
-        substract_supply(supply, 350, 75, 20, 1, 7)
+        substract_supply(supply, ammount_for_coffee(choice))
     elif choice == '3':
-        substract_supply(supply, 200, 100, 12, 1, 6)
+        substract_supply(supply, ammount_for_coffee(choice))
     elif choice == 'back':
         return False
     else:
